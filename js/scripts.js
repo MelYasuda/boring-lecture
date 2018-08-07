5275ef136bf9874fc85d94777a885baa8e0d3058
Mel Yasuda and Victoria Oh <victoria@>
2018-08-07 13:39:06 -0700

    add jquery to highlight paragraph when clicked

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $("p#pOne").click(function(){
+    $("p#pOne").removeClass();
+    $("p#pOne").addClass("borderline")
+  });
+
+  $("p#pTwo").click(function(){
+    $("p#pTwo").removeClass();
+    $("p#pTwo").addClass("borderline")
+  });
+
+  $("p#pThree").click(function(){
+    $("p#pThree").removeClass();
+    $("p#pThree").addClass("borderline")
+  });
+
+  $("p#pFour").click(function(){
+    $("p#pFour").removeClass();
+    $("p#pFour").addClass("borderline")
+  });
+
+  $("p#pFive").click(function(){
+    $("p#pFive").removeClass();
+    $("p#pFive").addClass("borderline")
+  });
+});
